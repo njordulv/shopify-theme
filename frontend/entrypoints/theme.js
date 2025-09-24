04e7553dff898394e84474d024e6d8464bd01234
njordulv <njordulv@gmail.com>
2025-09-24 22:11:46 +0300

    feat: faq section with alpine logic

--- frontend/entrypoints/theme.js
+++ frontend/entrypoints/theme.js
@@ -1,6 +1,12 @@
 import 'vite/modulepreload-polyfill'
 import { initProductVariants } from '@/scripts/product-variants'
 import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
+
+// Initialize Alpine
+window.Alpine = Alpine
+Alpine.plugin(collapse)
+Alpine.start()
 
 // Get the product data from Liquid
 const productDataElement = document.getElementById('product-data-json')
@@ -13,7 +19,3 @@ if (productDataElement) {
     console.error('Error parsing product data:', error)
   }
 }
-
-// Initialize Alpine
-window.Alpine = Alpine
-Alpine.start()
